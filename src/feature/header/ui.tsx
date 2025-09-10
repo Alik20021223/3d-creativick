ca5ed5b675e3621070945eaa3dcaac59bee1f239
Alisher <alik20021223@gmail.com>
2025-09-10 13:20:30 +0500

    did do fix tech page

--- src/feature/header/ui.tsx
+++ src/feature/header/ui.tsx
@@ -2,6 +2,7 @@ import { Button } from '@shadcn/button';
 import { Link, useLocation } from 'react-router-dom';
 import logoSrc from '@assets/logo-3d.svg';
 import userSrc from '@assets/user-profile.svg';
+import { ShoppingCart } from 'lucide-react';
 import { useIsMobile } from '@app/hook/useMobile';
 import MobileHeader from './mobileHeader';
 import { useAppStore } from '@app/store';
@@ -51,9 +52,9 @@ export default function Header({ menuItems }: HeaderProps) {
       <div
         className={[
           'fixed inset-x-0 z-50 px-[38px] max-md:px-2.5',
-          'top-5 max-md:top-[56px]', // оставляем твой отступ сверху
+          'top-5 max-md:top-[20px]', // оставляем твой отступ сверху
           'transition-transform duration-300 will-change-transform',
-          hidden ? '-translate-y-[120%]' : 'translate-y-0',
+          hidden ? '-translate-y-[120%] max-md:-translate-y-[180%]' : 'translate-y-0',
         ].join(' ')}
         onMouseEnter={() => setHidden(false)}
       >
@@ -61,11 +62,11 @@ export default function Header({ menuItems }: HeaderProps) {
           className={[
             'h-16 w-full rounded-4xl bg-white p-2.5 pl-[42px] max-sm:px-2.5',
             'transition-shadow duration-300',
-            atTop ? 'shadow-xl' : 'shadow-2xl',
+            atTop ? 'shadow-none' : 'shadow-2xl',
           ].join(' ')}
         >
           <header className='flex justify-center'>
-            <div className='flex w-full max-w-[1540px] items-center justify-between'>
+            <div className='container-custom flex w-full items-center justify-between'>
               <div className='flex items-center max-md:pl-2.5'>
                 <Link to='/' aria-label='На главную'>
                   <img src={logoSrc} alt='logo' />
@@ -94,16 +95,42 @@ export default function Header({ menuItems }: HeaderProps) {
                     ))}
                   </nav>
 
-                  <div className='flex gap-2'>
-                    <Button
-                      variant='pink'
-                      className='bg-pink-active flex h-11 w-[83px] justify-center'
-                      asChild
-                    >
-                      <Link to={isAuth ? '/profile' : '/login'}>
-                        <img src={userSrc} alt='user' className='pt-[5px]' />
-                      </Link>
-                    </Button>
+                  <div className='flex gap-20'>
+                    <div className='flex flex-col items-center'>
+                      <a
+                        href='mailto:info@3dkreativik.ru'
+                        className='text-dark-blue text-lg leading-[130%] font-normal'
+                      >
+                        info@3dkreativik.ru
+                      </a>
+                      <a
+                        href='tel:+84959888282'
+                        className='text-dark-blue text-lg leading-[130%] font-normal'
+                      >
+                        8 (495) 988-82-82
+                      </a>
+                    </div>
+
+                    <div className='flex gap-3'>
+                      {isAuth && (
+                        <Button className='bg-primary relative flex h-11 w-[70px] !p-0 text-white'>
+                          <ShoppingCart className='!h-8 !w-8' />
+                          <div className='bg-pink-active absolute -top-2 -right-1.5 flex h-6 w-6 items-center justify-center rounded-full'>
+                            12
+                          </div>
+                        </Button>
+                      )}
+
+                      <Button
+                        variant='pink'
+                        className='bg-pink-active flex h-11 w-[83px] justify-center'
+                        asChild
+                      >
+                        <Link to={isAuth ? '/profile' : '/login'}>
+                          <img src={userSrc} alt='user' className='pt-[5px]' />
+                        </Link>
+                      </Button>
+                    </div>
                   </div>
                 </>
               )}
--- src/pages/tech-page.tsx
+++ src/pages/tech-page.tsx
@@ -6,36 +6,39 @@ const TechPage = () => {
   const isMobile = useIsMobile();
 
   return (
-    <section className='p-10 max-md:p-0 max-md:py-[60px]'>
-      <div className='mx-auto rounded-[80px] bg-white p-2.5 pb-20 md:p-10'>
-        <div className='grid items-center gap-10 max-md:gap-20 md:grid-cols-2'>
-          {/* Левая картинка с градиентной подложкой */}
-          <div className='relative w-full max-md:-top-2.5 max-md:max-h-[276px] max-md:max-w-[355px]'>
-            <img
-              src={isMobile ? MobileTechImg : techImg}
-              alt='Технические работы'
-              className='h-full w-full object-contain'
-              draggable={false}
-            />
+    <section className='px-6 py-10 max-md:px-0 max-md:py-[60px]'>
+      <div className='mx-auto max-w-[1540px] rounded-[80px] bg-white p-2.5 pb-20 md:p-10'>
+        <div className='grid w-full items-center gap-10 max-md:gap-20 md:grid-cols-2'>
+          {/* Левая картинка */}
+          <div className='flex h-full w-full justify-center'>
+            <div className='pointer-events-none relative w-full select-none max-md:-top-2.5 max-md:max-h-[276px] max-md:max-w-[355px]'>
+              <img
+                src={isMobile ? MobileTechImg : techImg}
+                alt='Технические работы'
+                className='w-full object-contain'
+                draggable={false}
+              />
+            </div>
           </div>
 
-          {/* Правая колонка: заголовок, текст и кнопки */}
+          {/* Правая колонка */}
           <div>
-            <h1 className='text-dark-blue text-[54px] leading-[110%] font-bold max-md:text-[32px] md:text-[44px]'>
+            <h1 className='text-dark-blue text-[32px] leading-[110%] font-bold md:text-[44px] xl:text-[54px]'>
               Технические работы
             </h1>
 
-            <p className='text-secondary-text mt-[22px] text-lg leading-[130%] font-normal max-md:text-base md:text-[16px]'>
+            <p className='text-secondary-text mt-[22px] text-base leading-[130%] font-normal md:text-[16px]'>
               Приятно, граждане, наблюдать, как акционеры крупнейших компаний преданы
               социально-демократической анафеме. Современные технологии достигли такого уровня, что
               сплочённость команды профессионалов обеспечивает актуальность поставленных обществом
               задач.
             </p>
 
-            <div className='mt-8 flex gap-5 max-md:flex-col max-md:items-center max-md:justify-center'>
+            <div className='relative z-10 mt-8 flex gap-5 max-md:flex-col max-md:items-center max-md:justify-center'>
               <a
                 href='https://t.me/'
                 target='_blank'
+                rel='noopener noreferrer'
                 className='bg-primary-active inline-flex h-[56px] w-[195px] items-center justify-center rounded-full text-[22px] font-normal text-white max-md:w-full max-md:text-lg'
               >
                 Телеграм
@@ -43,6 +46,7 @@ const TechPage = () => {
               <a
                 href='https://vk.com/'
                 target='_blank'
+                rel='noopener noreferrer'
                 className='bg-primary-active inline-flex h-[56px] w-[195px] items-center justify-center rounded-full text-[22px] font-normal text-white max-md:w-full max-md:text-lg'
               >
                 ВКонтакте
